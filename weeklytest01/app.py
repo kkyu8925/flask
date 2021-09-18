a56d66281177a50765c1bed9bdea7897b65ebac3
gusrb8925 <gusrb8925@naver.com>
2021-09-18 19:49:57 +0900

    21-09-18 rename prj

--- weeklytest01/app.py
+++ weeklytest01/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-from pymongo import MongoClient
 
 client = MongoClient('mongodb://myUser:1234@3.35.246.21:27017/MyDB')
 db = client.MyDB
 
-import requests
-from bs4 import BeautifulSoup
-
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 
